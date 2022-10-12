25f60466a3e055f912468bb49101a23a97d9ca7e
María Valderrama <mavalroot@gmail.com>
2022-10-12 18:34:46 +0200

    test

---
+++ src/lib/icon/Icon.svelte
@@ -0,0 +1,15 @@
+<svelte:options tag="my-icon" />
+
+<script lang="ts">
+  import { icons } from "./paths";
+
+  export let icon: string | undefined;
+  export let size: string = '24';
+  export let fill: string | undefined = undefined;
+</script>
+
+{#if !!icon}
+  <svg width={size} height={size} viewBox="0 0 24 24"
+    ><path d={icons[icon]} {fill} /></svg
+  >
+{/if}
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,3 @@
 export * from './lib/Counter.svelte'
 export * from './lib/button/Button.svelte'
+export * from './lib/icon/Icon.svelte'
