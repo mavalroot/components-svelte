20b020de9bd6b1c73940671f86ef2a8d7b8b5574
María Valderrama <mavalroot@gmail.com>
2022-10-16 15:17:38 +0200

    feat: icon list

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
 export * from './lib/section/Section.svelte';
 export * from './lib/button/Button.svelte';
 export * from './lib/icon/Icon.svelte';
+export * from './shared/icon-list/IconList.svelte';
---
+++ src/shared/icon-list/IconList.svelte
@@ -0,0 +1,35 @@
+<svelte:options tag="icon-list" />
+
+<script lang="ts">
+  import '../style/tailwind.css';
+
+  import { icons } from '$lib/icon/paths';
+  import Icon from '$lib/icon/Icon.svelte';
+
+  let labels = Object.keys(icons);
+</script>
+
+<div class="grid">
+  {#each labels as label}
+    <div>
+      <Icon icon={label} />
+      <div>{label}</div>
+    </div>
+  {/each}
+</div>
+
+<style lang="scss">
+  .grid {
+    display: grid;
+    width: 100%;
+    margin: auto;
+    grid-template-columns: repeat(auto-fill, 9em);
+    grid-gap: 10px;
+
+    > div {
+      text-align: center;
+      border: 1px solid black;
+      padding: 5px;
+    }
+  }
+</style>
